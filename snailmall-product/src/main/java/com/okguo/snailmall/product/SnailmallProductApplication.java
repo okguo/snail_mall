129b33c3d15ea13c84f99a1e364bab49776ef8ac
Guoyongfu <Guoyongfu.1@hotmail.com>
2021-01-07 15:03:24 +0800

    配置mybatisplus

--- snailmall-product/src/main/java/com/okguo/snailmall/product/SnailmallProductApplication.java
+++ snailmall-product/src/main/java/com/okguo/snailmall/product/SnailmallProductApplication.java
@@ -1,9 +1,28 @@
 package com.okguo.snailmall.product;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+/**
+ * 1.整合mybatis-plus
+ *   1）、导入依赖
+ *			<dependency>
+ *             <groupId>com.baomidou</groupId>
+ *             <artifactId>mybatis-plus-boot-starter</artifactId>
+ *             <version>3.3.1</version>
+ *         </dependency>
+ *   2）、配置
+ *   	1、配置数据源
+ *   	  1）、导入数据源
+ *   	  2）、编辑配置文件
+ *		2、配置mybatis-plus
+ *		  1）、使用MapperScan
+ *		  2）、高祖mybatis-plus xml文件位置
+ */
+
 
 @SpringBootApplication
+@MapperScan("com.okguo.snailmall.product.dao")
 public class SnailmallProductApplication {
 
 	public static void main(String[] args) {
--- snailmall-product/src/test/java/com/okguo/snailmall/product/SnailmallProductApplicationTests.java
+++ snailmall-product/src/test/java/com/okguo/snailmall/product/SnailmallProductApplicationTests.java
@@ -1,13 +1,26 @@
 package com.okguo.snailmall.product;
 
+import com.okguo.snailmall.product.entity.BrandEntity;
+import com.okguo.snailmall.product.service.BrandService;
 import org.junit.jupiter.api.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
 
+@RunWith(SpringRunner.class)
 @SpringBootTest
 class SnailmallProductApplicationTests {
 
+	@Autowired
+	BrandService brandService;
+
 	@Test
 	void contextLoads() {
+		BrandEntity brandEntity = new BrandEntity();
+		brandEntity.setDescript("aaa");
+		brandEntity.setName("华为");
+		brandService.save(brandEntity);
 	}
 
 }
