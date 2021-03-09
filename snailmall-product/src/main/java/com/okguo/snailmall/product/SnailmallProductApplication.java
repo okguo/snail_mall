1b52e122face8e1563eb6a0a751e4aa159af2437
Guoyongfu <Guoyongfu.1@hotmail.com>
2021-03-09 18:55:37 +0800

    SpringSession

--- snailmall-product/src/main/java/com/okguo/snailmall/product/SnailmallProductApplication.java
+++ snailmall-product/src/main/java/com/okguo/snailmall/product/SnailmallProductApplication.java
@@ -6,6 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
+import org.springframework.session.data.redis.config.annotation.web.http.EnableRedisHttpSession;
 
 /**
  * 1.整合mybatis-plus
@@ -24,6 +25,7 @@ import org.springframework.cloud.openfeign.EnableFeignClients;
  *		  2）、高祖mybatis-plus xml文件位置
  */
 
+@EnableRedisHttpSession
 @EnableCaching
 @EnableFeignClients(basePackages = "com.okguo.snailmall.product.feign")
 @SpringBootApplication
