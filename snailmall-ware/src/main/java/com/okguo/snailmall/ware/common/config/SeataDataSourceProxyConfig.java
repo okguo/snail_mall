e3da4b14bb827c3df15230cf985899a47ef903f2
guoyongfu <Guoyongfu.1@hotmail.com>
2021-03-26 00:20:20 +0800

    seata集成

--- snailmall-ware/src/main/java/com/okguo/snailmall/ware/common/config/SeataDataSourceProxyConfig.java
+++ snailmall-ware/src/main/java/com/okguo/snailmall/ware/common/config/SeataDataSourceProxyConfig.java
@@ -1,20 +1,14 @@
-package com.okguo.snailmall.order.config;
+package com.okguo.snailmall.ware.common.config;
 
 import com.alibaba.druid.pool.DruidDataSource;
 import io.seata.rm.datasource.DataSourceProxy;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceProperties;
-import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 import javax.sql.DataSource;
 
-/**
- * @Description:
- * @Author: Guoyongfu
- * @Date: 2021/03/25 20:24
- */
 @Configuration
 public class SeataDataSourceProxyConfig {
 
